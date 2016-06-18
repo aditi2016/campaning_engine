a7d188b2ffb975c83b7bb8b3f9fc4aec5e244264
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-18 10:00:49 +0530

    sadded cron

---
+++ cron/reports.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spider-ninja
+ * Date: 6/18/16
+ * Time: 9:58 AM
+ */
+
+/*SELECT STATUS , count( * )
+FROM `campaning_logs`
+WHERE `campaning_id` =10
+GROUP BY STATUS */
+
+require_once "../library/sendEmail.php";
+require_once "../library/sms.php";
+require_once "../config/db_connection.php";
+
+
+$sql = "SELECT STATUS , count( * )
+FROM `campaning_logs`
+WHERE `campaning_id` =10
+GROUP BY STATUS";
+$result = mysqli_query($db, $sql);
+
+$camp = mysqli_fetch_assoc($result);
+
+sendMail("rahul_lahoria@yahoo.com", "camp report", json_encode($camp));
