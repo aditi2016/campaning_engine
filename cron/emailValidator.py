99596e2f905c403b696f0e70a12590b521cb0023
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-23 10:37:05 +0530

    sadded cron

---
+++ cron/emailValidator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import MySQLdb
+from validate_email import validate_email
+
+
+
+def validateEmailId( email ):
+  if (!validate_email(email) && !validate_email(email,check_mx=True) && !validate_email(email,verify=True)):
+    return false
+      
+  return true
+
+db = MySQLdb.connect("localhost","root","redhat@11111p","campaning_engine" )
+
+cursor = db.cursor()
+
+sql = "SELECT * FROM emails where status != 'valid' "
+
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      id = row[0]
+      email = row[1]
+      newStatus =  validateEmailId ( email )
+      # Now print fetched result
+      print "id=%s,email=%s,new status=%s" % \
+             (id, email, newStatus )
+except:
+   print "Error: unable to fecth data"
+
+# disconnect from server
+db.close()
