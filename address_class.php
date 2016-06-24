35c05d0a064d7867feee5abef092772cffdeecbe
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-24 12:48:08 +0530

    sadded cron

--- address_class.php
+++ address_class.php
@@ -6,6 +6,16 @@
  * Time: 12:19 PM
  */
 
+require_once "config/db_connection.php";
+
+if(isset($_GET['id'])){
+
+    $sql = "update words set type = '".$_GET['type']."' WHERE id = '".$_GET['id']."'" ;
+
+    mysqli_query($db, $sql);
+
+}
+
 session_start();
 if (!isset($_SESSION['user_id']))
     header("location: login.php");
@@ -13,12 +23,16 @@ if (!isset($_SESSION['user_id']))
 $userID = $_SESSION['user_id'];
 
 
-require_once "config/db_connection.php";
+
 
 $sql = "SELECT * FROM `words` WHERE type = 'new'" ;
 $result = mysqli_query($db, $sql);
 
 while ($word = mysqli_fetch_assoc($result)){
+
+    echo $word['word']
+        . "<a href='address_class.php?id=".$word['id']."&type=economic' >economic</a>"
+        . "<a href='address_class.php?id=".$word['id']."&type=business' >business</a><br/>";
 
 
 
