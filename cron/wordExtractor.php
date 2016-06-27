0f48621dee81714c46d64530c4356eddebd85216
aditi2016 <aditilahoria@gmail.com>
2016-06-27 11:50:37 +0530

    count

--- cron/wordExtractor.php
+++ cron/wordExtractor.php
@@ -10,13 +10,14 @@ $result = mysqli_query($db, $sql);
 while ($address = mysqli_fetch_assoc($result)){
 
     $words = explode(" ",$address['address']);
-   
+     
 
-    foreach($words as $word){
+
+    foreach($words as count($word)) {
 
         $sql = "INSERT INTO `campaning_engine`.`words` (`id`, `word`, `type`, `creation`) VALUES (NULL, '".$word."', 'new', CURRENT_TIMESTAMP);";
         mysqli_query($db, $sql);
- echo count($words);
+ 
     }
 
 
