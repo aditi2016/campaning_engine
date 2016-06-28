182c27e2eaabbf9e676e7224c3f52c54446ee9fa
aditi2016 <aditilahoria@gmail.com>
2016-06-28 16:09:37 +0530

    count

--- address_class.php
+++ address_class.php
@@ -5,36 +5,43 @@
  * Date: 6/24/16
  * Time: 12:19 PM
  */
-
-require_once "config/db_connection.php";
-
-if(isset($_GET['id'])){
-
-    $sql = "update words set type = '".$_GET['type']."' WHERE id = '".$_GET['id']."'" ;
-
-    mysqli_query($db, $sql);
-
-}
-
 session_start();
 if (!isset($_SESSION['user_id']))
     header("location: login.php");
 
 $userID = $_SESSION['user_id'];
 
+require_once "config/db_connection.php";
+$ignore = mysqli_query($db, "SELECT id FROM `words` WHERE type = 'ignore'");
+$ignorecount = mysqli_num_rows($ignore);
+
+$business = mysqli_query($db, "SELECT id FROM `words` WHERE type = 'business'");
+$businesscount = mysqli_num_rows($business);
+
+$economic = mysqli_query($db, "SELECT id FROM `words` WHERE type = 'economic'");
+$economiccount = mysqli_num_rows($economic);
+
+echo "Economic :".$economiccount."<br/>Business :".$businesscount."<br/>Ignore :".$ignorecount."<br/>";
 
 
+if(isset($_GET['id'])){
 
-$sql = "SELECT * FROM `words` WHERE type = 'new' limit 0,100" ;
-$result = mysqli_query($db, $sql);
+    $sql = "update words set type = '".$_GET['type']."' WHERE id = '".$_GET['id']."'" ;
 
-while ($word = mysqli_fetch_assoc($result)){
+    mysqli_query($db, $sql);
+    header("Location:address_class.php");
+}
+else {
+	$sql = "SELECT * FROM `words` WHERE type = 'new' limit 0,100" ;
+	$result = mysqli_query($db, $sql);
+//$data=mysql_fetch_assoc($result);
+//echo $data['total'];
 
-    echo count($word,1);['word']
-        . "&nbsp &nbsp &nbsp <a href='address_class.php?id=".$word['id']."&type=economic' >economic</a> &nbsp &nbsp &nbsp"
-        . "<a href='address_class.php?id=".$word['id']."&type=business' >business</a>&nbsp &nbsp &nbsp "
-        . "<a href='address_class.php?id=".$word['id']."&type=ignore' >Ignore</a><br/> ";
+	while ($word = mysqli_fetch_assoc($result)){
 
-
-
+	    echo $word['word']
+	        . "&nbsp &nbsp &nbsp <a href='address_class.php?id=".$word['id']."&type=economic' >economic</a> &nbsp &nbsp &nbsp"
+	        . "<a href='address_class.php?id=".$word['id']."&type=business' >business</a>&nbsp &nbsp &nbsp "
+	        . "<a href='address_class.php?id=".$word['id']."&type=ignore' >Ignore</a><br/> ";
+	}
 }
