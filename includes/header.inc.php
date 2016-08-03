56135a75717eb1b8b4e14012bc164ad9a3f3be68
rahullahoria <rahul_lahoria@yahoo.com>
2016-08-03 13:43:06 +0530

    sadded cron

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,9 +1,10 @@
 
 
 <header>
-<center>
+<div style="text-align: center;">
+    <a href="http://shatkonlabs.com/cold_call/">Cold Colling<a>|
 <a href="sms_camp.php">SMS CAMP<a>|<a href="email_camp.php">E-Mail CAMP<a>|<a href="create_camp.php">Create CAMP<a>
                         |<a href="address_class.php">Address Classifier<a>
-</center>
+</div>
 
 <header>
