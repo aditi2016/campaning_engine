80ce41c413fc8511cbf195ffe992ebff750f17e7
root <root@localhost.localdomain>
2016-08-06 12:11:31 +0530

    mailer admin

--- mailerAdmin.php
+++ mailerAdmin.php
@@ -1,15 +1,5 @@
 <?php
 
-session_start();
-if (!isset($_SESSION['user_id']))
-    header("location: login.php");
-
-$userID = $_SESSION['user_id'];
-
-$db_handle = mysqli_connect("localhost","root","redhat@11111p","gmail_mailer");
-
-$result = mysqli_query($db_handle,"select * from users where status = 'in-active';");
-
 if(isset($_GET['getActive'])){
 	$result = mysqli_query($db_handle,"select * from users where status = 'active';");
 
@@ -32,10 +22,24 @@ if(isset($_GET['changeStatus']) && isset($_GET['id']) ){
 
 }
 
+session_start();
+if (!isset($_SESSION['user_id']))
+    header("location: login.php");
+
+$userID = $_SESSION['user_id'];
+
+$db_handle = mysqli_connect("localhost","root","redhat@11111p","gmail_mailer");
+
+$result = mysqli_query($db_handle,"select * from users where status = 'in-active';");
+
+
+
 while($row = mysqli_fetch_assoc($result)){
 
 	echo $row['username']. " status: in-active <a href=\"?changeStatus=active&id=".$row['id']."\">Activate</a><br/>";  
 
 
 }
+
+mysqli_close($db_handle);
 ?>
